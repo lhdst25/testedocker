6a1e16f704ae5c8ee04a13890d5b25312dc22d91
lhdst25 <41303034+lhdst25@users.noreply.github.com>
2019-03-25 15:44:22 -0300

    main update

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import os
 import json
 import flask
-import base64
-import numpy               as     np
-import tensorflow          as     tf
-from   swiftclient.service import Connection
+import numpy as np
+import tensorflow as tf
 from pattern_recognition import main_patterns
 from pattern_prediction import main_predict_pattern
 import jsonpickle
-import pandas as pd
 import pickle
 
-app       = flask.Flask(__name__)
+app = flask.Flask(__name__)
 app.debug = False
-graph     = tf.Graph()
-labels    = []
+graph = tf.Graph()
+labels = []
 
 
 @app.route('/init', methods=['POST'])
@@ -23,12 +20,11 @@ def init():
     try:
 
         message = flask.request.get_json(force=True, silent=True)
-        
+
         if message and not isinstance(message, dict):
             flask.abort(404)
 
         print(message)
-
 
     except Exception as e:
         print("Error in downloading content")
@@ -42,8 +38,10 @@ def init():
 @app.route('/run', methods=['POST'])
 def run():
     print(tf.__version__)
+
     def error():
-        response = flask.jsonify({'error': 'The action did not receive a dictionary as an argument.'})
+        response = flask.jsonify(
+            {'error': 'The action did not receive a dictionary as an argument.'})
         response.status_code = 404
         return response
 
@@ -55,65 +53,67 @@ def run():
         args = message.get('value', {}) if message else {}
 
         if not isinstance(args, dict):
-        
+
             return error()
-        
+
         dict_encode = {}
 
         if int(args["type"]) == 1:
-            
+
             try:
                 dict_out = main_patterns(args)
-                
+
                 params = ['minSamples', 'samplesDistance', 'epsAll',
-                      'slopeAngle', 'predictOutlier', 'threshold', 'mse',
-                      'predictPattern', 'scalerAnomaly', 'modelAnomaly',
-                      'modelPattern', 'nClusters', 'resultsPattern']
-                
-                
+                          'slopeAngle', 'predictOutlier', 'threshold', 'mse',
+                          'predictPattern', 'scalerAnomaly', 'modelAnomaly',
+                          'modelPattern', 'nClusters', 'resultsPattern']
+
                 for i in params:
-                    dict_encode[i] = jsonpickle.encode(pickle.dumps(dict_out[i]))
+                    dict_encode[i] = jsonpickle.encode(
+                        pickle.dumps(dict_out[i]))
+
+                dict_encode["deviceId"] = args["deviceId"]
+                dict_encode["fwVersion"] = args["fwVersion"]
                 
                 jsondictOut = jsonpickle.encode(dict_encode)
                 answer = {"msg": jsondictOut}
                 response = flask.jsonify(answer)
                 response.status_code = 200
-                
+
                 return response
-            
+
             except Exception as error:
                 return {'error': str(error)}
-                
+
         elif int(args["type"]) == 2:
-            
+
             try:
                 keys = ['threshold', 'scalerAnomaly', 'modelAnomaly',
-                'modelPattern']
-            
+                        'modelPattern']
+
                 dict_in = {}
-            
+
                 for i in keys:
                     dict_in[i] = pickle.loads(jsonpickle.decode(args[i]))
-            
+
                 dict_in["jsons"] = args["jsons"]
-            
+
                 pred = main_predict_pattern(dict_in)
-            
+
                 print(pred)
                 for i, j in pred.items():
                     pred[i] = j.tolist()
-            
+
                 jsondictOut1 = json.dumps(pred)
-                
+
                 answer = {"msg": jsondictOut1}
                 response = flask.jsonify(answer)
                 response.status_code = 200
-                
+
                 return response
-    
+
             except Exception as error:
-                return {'error': str(error)}     
-            
+                return {'error': str(error)}
 
 
 if __name__ == '__main__':
